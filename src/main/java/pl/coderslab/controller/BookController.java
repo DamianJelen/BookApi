2a729f36f32ac6bc1a8950e8ab3ec20326a2d367
Damian Jeleń <damian.jelen.1986@gmail.com>
2023-01-28 00:40:06 +0100

    finish book controller

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,13 +1,44 @@
 package pl.coderslab.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.pojo.Book;
+import pl.coderslab.service.MockBookService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
-    @RequestMapping("/helloBook")
-    public String helloBook() {
-        return "Hello book";
+    private MockBookService bookService;
+
+    @Autowired
+    public BookController(MockBookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("")
+    public List<Book> getBooks() {
+        return bookService.getBooks();
+    }
+
+    @GetMapping("/{bookId}")
+    public Book getBookById(@PathVariable Long bookId) {
+        return bookService.getBookById(bookId);
+    }
+
+    @PostMapping("")
+    public void addBook(@RequestBody Book book) {
+        bookService.addBook(book);
+    }
+
+    @DeleteMapping("/{bookId}")
+    public void deleteBook(@PathVariable Long bookId) {
+        bookService.delBook(bookId);
+    }
+
+    @PutMapping("")
+    public void modifyBook(@RequestBody Book mBook) {
+        bookService.modBook(mBook);
     }
 }
